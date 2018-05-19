e8b945f38f8cd7aba551d5bfbde227f9201d09c6
Ian Hopkins <ian@lucidhelix.com>
2018-05-19 07:18:46 -0700

    Update the US flags to be the correct size.

--- assets/img/flags/run.bat
+++ assets/img/flags/run.bat
@@ -1,4 +1,205 @@
 convert -size 32x32 xc:none background.png
 
-composite -geometry +1+6 state/US/AL.png background.png temp.png
-composite overlay.png temp.png AL.png
+convert state/US/flat/AL.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png AL.png
+    
+convert state/US/flat/AK.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png AK.png
+    
+convert state/US/flat/AZ.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png AZ.png
+    
+convert state/US/flat/AR.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png AR.png
+    
+convert state/US/flat/CA.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png CA.png
+    
+convert state/US/flat/CO.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png CO.png
+    
+convert state/US/flat/CT.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png CT.png
+    
+convert state/US/flat/DE.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png DE.png
+    
+convert state/US/flat/DC.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png DC.png
+    
+convert state/US/flat/FL.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png FL.png
+    
+convert state/US/flat/GA.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png GA.png
+    
+convert state/US/flat/HI.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png HI.png
+    
+convert state/US/flat/ID.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png ID.png
+    
+convert state/US/flat/IL.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png IL.png
+    
+convert state/US/flat/IN.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png IN.png
+    
+convert state/US/flat/IA.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png IA.png
+    
+convert state/US/flat/KS.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png KS.png
+    
+convert state/US/flat/KY.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png KY.png
+    
+convert state/US/flat/LA.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png LA.png
+    
+convert state/US/flat/ME.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png ME.png
+    
+convert state/US/flat/MD.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png MD.png
+    
+convert state/US/flat/MA.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png MA.png
+    
+convert state/US/flat/MI.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png MI.png
+    
+convert state/US/flat/MN.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png MN.png
+    
+convert state/US/flat/MS.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png MS.png
+    
+convert state/US/flat/MO.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png MO.png
+    
+convert state/US/flat/MT.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png MT.png
+    
+convert state/US/flat/NE.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png NE.png
+    
+convert state/US/flat/NV.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png NV.png
+    
+convert state/US/flat/NH.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png NH.png
+    
+convert state/US/flat/NJ.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png NJ.png
+    
+convert state/US/flat/NM.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png NM.png
+    
+convert state/US/flat/NY.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png NY.png
+    
+convert state/US/flat/NC.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png NC.png
+    
+convert state/US/flat/ND.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png ND.png
+    
+convert state/US/flat/OH.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png OH.png
+    
+convert state/US/flat/OK.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png OK.png
+    
+convert state/US/flat/OR.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png OR.png
+    
+convert state/US/flat/PA.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png PA.png
+    
+convert state/US/flat/RI.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png RI.png
+    
+convert state/US/flat/SC.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png SC.png
+    
+convert state/US/flat/SD.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png SD.png
+    
+convert state/US/flat/TN.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png TN.png
+    
+convert state/US/flat/TX.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png TX.png
+    
+convert state/US/flat/UT.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png UT.png
+    
+convert state/US/flat/VT.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png VT.png
+    
+convert state/US/flat/VA.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png VA.png
+    
+convert state/US/flat/WA.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png WA.png
+    
+convert state/US/flat/WV.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png WV.png
+    
+convert state/US/flat/WI.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png WI.png
+    
+convert state/US/flat/WY.png -resize 30x20! flag-a.png
+composite -geometry +1+6 flag-a.png background.png flag-b.png
+composite overlay.png flag-b.png WY.png
